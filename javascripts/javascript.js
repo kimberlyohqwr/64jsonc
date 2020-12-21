c796c5530ff4219ae93117cf6931bacb07e30359
kimberlyohqwr <adelinarak4sh@list.ru>
2020-12-22 07:37:42 +0800

    fourth commit

--- javascripts/javascript.js
+++ javascripts/javascript.js
@@ -1,55 +1,39 @@
-$(document).ready(function() {
+$(window).load(function () {
     var z_index = 0;
-    var relocate = function(row, col, xh, xw, mh, mw) {
+    var relocate = function (row, col, xh, xw, mh, mw) {
         var $fake_cell = $('.fake-row:eq(' + row + ') .fake-col:eq(' + col + ')');
         var offset = $fake_cell.offset();
         var cell = '.cell' + (row + 1) + '' + (col + 1);
-        var animating = false;
         $(cell).css({
             'width': $fake_cell.width(),
             'height': $fake_cell.height(),
             'top': offset.top,
-            'left': offset.left
+            'left': offset.left,
+            'flex-flow': xh > 1 ? 'column' : 'row'
         });
-        $(cell + ' .fixed').css({
+        $(cell + ' .icon').css({
             'width': $fake_cell.width(),
-            'height': $fake_cell.height(),
+            'height': $fake_cell.height()
         });
-        $(cell + ' > div').css('display', xh > 1 ? 'table-row' : 'table-cell');
-        $(cell).hover(function() {
-            //            if(animating) return;
-            $(this).css('z-index', ++z_index);
-            $(this).animate({
+        $(cell).hover(function () {
+            $(cell).css('z-index', ++z_index);
+            $(cell).animate({
                 'width': $fake_cell.width() * xw,
                 'height': $fake_cell.height() * xh,
                 'margin-left': $fake_cell.height() * mw,
                 'margin-top': $fake_cell.height() * mh
-            });
-        }, function() {
-            //            if(animating) return;
-            animating = true;
-            $(this).animate({
+            }, {queue: false, duration: 500});
+        }, function () {
+            $(cell).animate({
                 'width': $fake_cell.width(),
                 'height': $fake_cell.height(),
                 'margin-left': 0,
                 'margin-top': 0
-            }, function() {
-                animating = false;
-            });
+            }, {queue: false, duration: 500});
         });
     };
-    var onResize = function() {
-        var width = $(window).width();
-        var height = $(window).height();
-        var min = (width < height ? width : height) - 16;
-        $('.fake-grid').css({
-            'width': min,
-            'height': min,
-            'top': (height - min) / 2,
-            'left': (width - min) / 2
-        });
-
-        relocate(0, 0, 1, 2);
+    var relocateAll = function () {
+        relocate(0, 0, 2, 1);
         relocate(0, 1);
         relocate(0, 2, 2, 1);
         relocate(1, 0);
@@ -57,8 +41,28 @@ $(document).ready(function() {
         relocate(1, 2);
         relocate(2, 0, 2, 1, -1);
         relocate(2, 1);
-        relocate(2, 2, 2, 1, -1);
+        relocate(2, 2, 1, 2, 0, -1);
+    };
+    var onResize = function () {
+        var width = $(window).width();
+        var height = $(window).height();
+        var min = (width < height ? width : height) - 16;
+        $('.fake-grid').animate({
+            'width': min,
+            'height': min,
+            'top': (height - min) / 2,
+            'left': (width - min) / 2
+        }, 0, null, function () {
+            relocateAll();
+        });
     };
     onResize();
     $(window).resize(onResize);
+
+    var months = new Array("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
+    var date = new Date();
+    var day = date.getDate();
+    var month = date.getMonth();
+    var year = date.getFullYear();
+    $('#today').text(months[month] + ' ' + day + ', ' + year);
 });
