80811450ee274653d42bc8f885e2ba1f8e8ee72d
kimberlyohqwr <adelinarak4sh@list.ru>
2022-01-06 02:01:37 +0800

    Change router to HashRouter

--- src/components/Screen/index.js
+++ src/components/Screen/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import './stylesheet.scss';
 import { Desktop, Taskbar } from 'components';
 import { FileSystemContext, ResponsiveContext } from 'contexts';
