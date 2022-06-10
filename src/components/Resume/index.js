442cecc51826c6b12f0b342f538a456de4f41ab8
kimberlyohqwr <adelinarak4sh@list.ru>
2022-06-11 02:13:48 +0800

    Update resume (major changes)

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -15,16 +15,16 @@ function Resume() {
           <div className="name">{bio.full_name}</div>
           <div className="bar">
             <a className="item" href={bio.links.github}>
-              <FontAwesomeIcon fixedWidth icon={faGithub}/>
+              <FontAwesomeIcon fixedWidth icon={faGithub} />
               <span>{removeProtocol(bio.links.github)}</span>
             </a>
             <a className="item"
                href={`https://www.google.com/maps/place/${encodeURI(bio.location)}`}>
-              <FontAwesomeIcon fixedWidth icon={faMapMarkerAlt}/>
+              <FontAwesomeIcon fixedWidth icon={faMapMarkerAlt} />
               <span>{bio.location}</span>
             </a>
             <a className="item" href={`mailto:${bio.email}`}>
-              <FontAwesomeIcon fixedWidth icon={faEnvelope}/>
+              <FontAwesomeIcon fixedWidth icon={faEnvelope} />
               <span>{bio.email}</span>
             </a>
           </div>
@@ -35,9 +35,8 @@ function Resume() {
               <div className="title">Summary</div>
               <div className="content">
                 <div className="item">
-                  <div className="primary paragraph">
-                    Software Engineer with 3.5 years of work experience. Open source projects and hackathons keep myself
-                    up to date with the latest tech trends.
+                  <div className="row paragraph">
+                    {bio.description}
                   </div>
                 </div>
               </div>
@@ -46,27 +45,48 @@ function Resume() {
               <div className="title">Education</div>
               <div className="content">
                 <div className="item">
-                  <div className="primary">Georgia Institute of Technology</div>
-                  <div className="row">BS in <b>Computer Science</b></div>
-                  <div className="row">Expected <b>May 2021</b></div>
-                  <div className="row">Concentration in Theory and Intelligence</div>
-                  <div className="row">Minor in Economics</div>
+                  <div className="row">Georgia Institute of Technology</div>
+                  <div className="bullet">BS in <b>Computer Science</b></div>
+                  <div className="bullet">Expected <b>May 2021</b></div>
+                  <div className="bullet">Concentration in Theory and Intelligence</div>
+                  <div className="bullet">Minor in Economics</div>
                 </div>
                 <div className="item">
-                  <div className="primary">National University of Singapore</div>
-                  <div className="row">Exchange in Fall 2019</div>
+                  <div className="row">National University of Singapore</div>
+                  <div className="bullet">Exchange in Fall 2019</div>
                 </div>
               </div>
             </div>
             <div className="section">
-              <div className="title">Coursework</div>
+              <div className="title">Awards</div>
               <div className="content">
                 <div className="item">
-                  {
-                    bio.coursework.map(course => (
-                      <div className="row" key={course}>{course}</div>
-                    ))
-                  }
+                  <div className="row">The Home Depot Hackathon</div>
+                  <div className="bullet">2020, First Place</div>
+                </div>
+                <div className="item">
+                  <div className="row">Chengdu 80 Fintech Product Design & Research Competition</div>
+                  <div className="bullet">2019, Second Place</div>
+                </div>
+                <div className="item">
+                  <div className="row">T-Mobile Hackathon</div>
+                  <div className="bullet">2019, First Place</div>
+                </div>
+                <div className="item">
+                  <div className="row">Consortium for Computing Sciences in Colleges - Midwest</div>
+                  <div className="bullet">2017, First Place in Student Showcase</div>
+                </div>
+                <div className="item">
+                  <div className="row">USA Computing Olympiad</div>
+                  <div className="bullet">2015, Platinum Division</div>
+                </div>
+                <div className="item">
+                  <div className="row">Korea Youth Science of Information Olympiad</div>
+                  <div className="bullet">2011, First Place</div>
+                </div>
+                <div className="item">
+                  <div className="row">Korea Olympiad in Informatics</div>
+                  <div className="bullet">2010, Silver Medal</div>
                 </div>
               </div>
             </div>
@@ -76,24 +96,10 @@ function Resume() {
                 <div className="item">
                   {
                     Object.entries(bio.skills).map(([skill, level]) => (
-                      <div className="primary" key={skill}>
-                        <div className="name">{skill}</div>
-                        <div className="level">{level}</div>
-                      </div>
-                    ))
-                  }
-                </div>
-              </div>
-            </div>
-            <div className="section">
-              <div className="title">Languages</div>
-              <div className="content">
-                <div className="item">
-                  {
-                    Object.entries(bio.languages).map(([language, level]) => (
-                      <div className="primary" key={language}>
-                        <div className="name">{language}</div>
-                        <div className="level">{level}</div>
+                      <div className="row" key={skill}>
+                        <div className="primary">{skill}</div>
+                        <div className="spacer" />
+                        <div className="secondary">{level}</div>
                       </div>
                     ))
                   }
@@ -106,57 +112,90 @@ function Resume() {
               <div className="title">Work Experience</div>
               <div className="content">
                 <div className="item">
-                  <div className="primary">
-                    <div className="name">SWE Intern at Google</div>
-                    <div className="location">Mountain View, CA (Remote)</div>
-                    <div className="date">May 2020 - Aug. 2020</div>
+                  <div className="row">
+                    <div className="primary">True Shape (d.b.a. Gatherly)</div>
+                    <div className="spacer" />
+                    <div className="secondary">Wilmington, DE</div>
                   </div>
                   <div className="row">
-                    Explored improvements to the accessibility of data visualization with an aim to merge the features
-                    into Google Analytics (GA).
+                    <div className="secondary">Remote Software Engineer (Part-Time)</div>
+                    <div className="spacer" />
+                    <div className="secondary">Jan. 2021 - Current</div>
                   </div>
                   <div className="row">
-                    Designed the data flow and component hierarchy of the Angular app to align with GA, and set up CI/CD
-                    pipelines to facilitate collaboration with teammates.
+                    <div className="secondary">Remote Contract Software Engineer</div>
+                    <div className="spacer" />
+                    <div className="secondary">Oct. 2020 - Jan. 2021</div>
                   </div>
-                  <div className="row">
-                    Implemented data audification of line charts using the Web Audio API.
+                  <div className="bullet">
+                    Fully rewrote a virtual conference platform written in React and Node.js to be more performant and robust with the help of end-to-end, integration, and load testing.
                   </div>
-                  <div className="row">
-                    Practiced lazy loading to minimize the anticipated performance impact on GA.
+                  <div className="bullet">
+                    Created a TypeScript package to define interfaces of REST API and WebSocket payloads to be shared by the backend and frontend packages.
+                  </div>
+                  <div className="bullet">
+                    Implemented semantic versioning using Lerna and Conventional Commits, and integrated Sentry to facilitate error tracking and troubleshooting.
                   </div>
                 </div>
                 <div className="item">
-                  <div className="primary">
-                    <div className="name">SWE Intern at Randstad USA</div>
-                    <div className="location">Atlanta, GA</div>
-                    <div className="date">Jan. 2020 - Apr. 2020</div>
+                  <div className="row">
+                    <div className="primary">Google</div>
+                    <div className="spacer" />
+                    <div className="secondary">Mountain View, CA</div>
                   </div>
                   <div className="row">
+                    <div className="secondary">Remote Software Engineering Intern</div>
+                    <div className="spacer" />
+                    <div className="secondary">May 2020 - Aug. 2020</div>
+                  </div>
+                  <div className="bullet">
+                    Explored improvements to the accessibility of data visualization with an aim to merge the features into Google Analytics.
+                  </div>
+                  <div className="bullet">
+                    Designed data flow and component hierarchy of the Angular app, and used lazy loading to minimize the anticipated performance impact.
+                  </div>
+                  <div className="bullet">
+                    Set up CI/CD pipelines to automate linting, unit testing, and deployment.
+                  </div>
+                  <div className="bullet">
+                    Developed data audification of line charts using the Web Audio API.
+                  </div>
+                </div>
+                <div className="item">
+                  <div className="row">
+                    <div className="primary">Randstad Technologies</div>
+                    <div className="spacer" />
+                    <div className="secondary">Atlanta, GA</div>
+                  </div>
+                  <div className="row">
+                    <div className="secondary">Software Engineering Intern</div>
+                    <div className="spacer" />
+                    <div className="secondary">Jan. 2020 - Apr. 2020</div>
+                  </div>
+                  <div className="bullet">
                     Maintained a talent management system written in Angular and Node.js.
                   </div>
-                  <div className="row">
-                    Enhanced the use of TypeScript and simplified the data flow within the Angular app.
-                  </div>
-                  <div className="row">
-                    Logged inbound HTTP requests into BigQuery within the backend microservices.
+                  <div className="bullet">
+                    Rewrote TypeScript interfaces in a more object-oriented way, and simplified data flow within the Angular app.
                   </div>
                 </div>
                 <div className="item">
-                  <div className="primary">
-                    <div className="name">Software Engineer at Prendssoin</div>
-                    <div className="location">Seoul, South Korea</div>
-                    <div className="date">Feb. 2017 - Jan. 2020</div>
+                  <div className="row">
+                    <div className="primary">Prendssoin</div>
+                    <div className="spacer" />
+                    <div className="secondary">Seoul, South Korea</div>
                   </div>
                   <div className="row">
-                    Recruited 5 developers and led the team using Scrum.
+                    <div className="secondary">Software Engineer</div>
+                    <div className="spacer" />
+                    <div className="secondary">Feb. 2017 - Jan. 2020</div>
                   </div>
-                  <div className="row">
+                  <div className="bullet">
+                    Managed a Scrum team of five engineers to deliver multiple outsourced web services.
+                  </div>
+                  <div className="bullet">
                     Wrote boilerplates for React and Node.js projects in TypeScript, implementing dependency injection
                     and modularization in a monorepo.
-                  </div>
-                  <div className="row">
-                    Conducted a weekly study group on the topics of development and communication.
                   </div>
                 </div>
               </div>
@@ -165,87 +204,41 @@ function Resume() {
               <div className="title">Projects</div>
               <div className="content">
                 <div className="item">
-                  <div className="primary">
-                    <div className="name"><a href="https://github.com/64json/gt-scheduler">GT Scheduler</a></div>
-                    <div className="date">2018</div>
-                  </div>
                   <div className="row">
+                    <div className="primary"><a href="https://github.com/64json/gt-scheduler">GT Scheduler</a></div>
+                    <div className="spacer" />
+                    <div className="secondary">2018</div>
+                  </div>
+                  <div className="bullet">
                     A scheduler that helps Georgia Tech students find the best schedule among all the possible
                     combinations of courses.
                   </div>
-                  <div className="row">
-                    Used GitHub Pages to serve the static website built with React and ran the Node.js crawler
-                    periodically on Heroku.
+                  <div className="bullet">
+                    Used GitHub Pages to serve a React app, and GitHub Actions to periodically run a Node.js crawler.
                   </div>
-                  <div className="row">
+                  <div className="bullet">
                     Recorded 4k+ unique visitors during the Fall 2019 registration period.
                   </div>
                 </div>
                 <div className="item">
-                  <div className="primary">
-                    <div className="name"><a href="https://github.com/algorithm-visualizer">Algorithm Visualizer</a>
-                    </div>
-                    <div className="date">2016</div>
-                  </div>
                   <div className="row">
+                    <div className="primary"><a href="https://github.com/algorithm-visualizer">Algorithm Visualizer</a>
+                    </div>
+                    <div className="spacer" />
+                    <div className="secondary">2016</div>
+                  </div>
+                  <div className="bullet">
                     An interactive online platform that visualizes algorithms from code.
                   </div>
-                  <div className="row">
+                  <div className="bullet">
                     Wrote visualization libraries in JavaScript, C++, and Java.
                   </div>
-                  <div className="row">
-                    Used AWS Lambda to securely run user-submitted code at a low cost.
+                  <div className="bullet">
+                    Used AWS Lambda for running user-submitted code for the sake of security, scalability, and affordability.
                   </div>
-                  <div className="row">
-                    Collaborated with 40+ developers and gained 30k+ stars on GitHub.
+                  <div className="bullet">
+                    Collaborated with 40+ developers and gained 33k+ stars on GitHub.
                   </div>
-                </div>
-              </div>
-            </div>
-            <div className="section">
-              <div className="title">Awards</div>
-              <div className="content wrap">
-                <div className="item">
-                  <div className="primary">
-                    <div className="name">The Home Depot Hackathon</div>
-                    <div className="date">2020</div>
-                  </div>
-                  <div className="row">First Place</div>
-                </div>
-                <div className="item">
-                  <div className="primary">
-                    <div className="name">T-Mobile C2CHack</div>
-                    <div className="date">2019</div>
-                  </div>
-                  <div className="row">First Place</div>
-                </div>
-                <div className="item">
-                  <div className="primary">
-                    <div className="name">Developer Circles Community Challenge</div>
-                    <div className="date">2017</div>
-                  </div>
-                  <div className="row">Best Updated Pre-Existing App</div>
-                </div>
-                <div className="item">
-                  <div className="primary">
-                    <div className="name">Consortium for Computing Sciences in Colleges: Midwest</div>
-                    <div className="date">2017</div>
-                  </div>
-                  <div className="row">First Place in Student Showcase</div>
-                </div>
-                <div className="item">
-                  <div className="primary">
-                    <div className="name">USA Computing Olympiad</div>
-                    <div className="date">2015</div>
-                  </div>
-                  <div className="row">Platinum Division</div>
-                </div>
-                <div className="item">
-                  <div className="primary">
-                    <div className="name">Korea Olympiad in Informatics</div>
-                    <div className="date">2010</div>
-                  </div>
-                  <div className="row">Silver Medal</div>
                 </div>
               </div>
             </div>
